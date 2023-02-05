51ed8d5a07686544f0ca64f4a30465dceeea87a2
JordiDeep <101256152+JordiDeep@users.noreply.github.com>
2023-02-05 19:11:38 +0100

    Metode AfegirProductes acabat

--- BotigaCistella/Program.cs
+++ BotigaCistella/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Botiga
+            string[] productesBotiga;
+            productesBotiga = new string[20];
+            double[] preus;
+            preus = new double[20];
+            int nElemBotiga = 0;
+
+            //Cistella
+            string[] productesCistella;
+            productesCistella= new string[20];
+            int[] quantitats;
+            quantitats= new int[20];
+            int nElemCistella = 0;
+            int diners = 0;
+
+        }
+
+        static void AfegirProducte(string producte, double preu, string[] productes, double[] preus, ref int nElem)
+        {
+            if (nElem < productes.Length)
+            {
+                productes[nElem] = producte;
+                preus[nElem] = preu;
+                nElem++;
+            }
+            else
+            {
+                Console.Write("Vols ampliar la botiga?");
+            }
         }
     }
 }
